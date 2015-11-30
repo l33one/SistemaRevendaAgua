8e65331ebea12f5392414675e06f7272740ba39a
leeung <leeung@gmail.com>
2015-11-30 00:57:15 -0300

    concludo a altenticação do usuario

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?
+//starta a seção da aplicação
+session_start();
+
 include("cabecalho.php");
 include("rodape.php");
+
+/*verifica se existe aluem logado 
+ * se não, direciona para fazer o login;
+ * se sim, prossegue no processamento da pagina
+ */
+if(!isset($_SESSION['secao']) || $_SESSION['secao']['status'] != true) header("Location: login.php");
 
 ?>
 
 <h1>Sistema de Revenda de Água</h1>
+<h3>Olá <?php echo $_SESSION['secao']['usuario'];?>, Seja bem vindo.</h3>
+
--- valida-login.php
+++ valida-login.php
@@ -1,10 +1,40 @@
 <?
+session_start();
 include("cabecalho.php");
 include("rodape.php");
+include("conexao.php");
 
 $email = $_POST['email'];
 $senha = $_POST['senha'];
 
-echo $email . ' - ' . $senha;
+//prepara uma query
+$stmt = mysqli_prepare($conexao, "select nome from usuarios where email= ? and senha = ?");
+
+//passa os parametros para a query
+mysqli_stmt_bind_param($stmt, "ss", $email, $senha);
+
+//executa a query
+mysqli_stmt_execute($stmt);
+
+//informa qual variavel recebe o valor retornado da consulta
+mysqli_stmt_bind_result($stmt, $nome);
+
+/*verifica se a consulta retornou algo
+ * true se sim
+ * false se erro
+ * null se vazio
+ */
+if(mysqli_stmt_fetch($stmt)){
+	//se a consulta = true, cria a sessão logado e a define como true;
+	$_SESSION['logado'] = array('usuario'=> $nome, 'horaSecao'=> date("d/m/Y H:i:s"));
+}
+
+//fecha a conexao 
+mysqli_stmt_close($stmt);
+
+//direciona para index
+header("Location: index.php");
+
+
 ?>
 
